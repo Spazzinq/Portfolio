82810073a163f0eb8ad8ced608fad6c6c72acbd1
Spazzinq <26337405+Spazzinq@users.noreply.github.com>
2020-03-16 20:22:58 -0400

    First VC version

---
+++ resources/typing.js
@@ -0,0 +1,9 @@
+var options = {
+     stringsElement: '#typed-strings',
+     startDelay: 1000,
+     backDelay: 5000,
+     typeSpeed: 20,
+     smartBackspace: true
+ };
+
+var typed = new Typed('#typed', options);
