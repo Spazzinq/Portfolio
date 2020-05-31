5ffa5503def7af86e393fdead3484e16536e0b21
Spazzinq <26337405+Spazzinq@users.noreply.github.com>
2020-05-30 23:31:16 -0400

    Refactor, change colors

--- resources/typing.js
+++ resources/typing.js
@@ -1,9 +1,9 @@
 var options = {
-     stringsElement: '#typed-strings',
+     stringsElement: '.typed-strings',
      startDelay: 1000,
      backDelay: 5000,
      typeSpeed: 20,
      smartBackspace: true
  };
 
-var typed = new Typed('#typed', options);
+var typed = new Typed('.typed', options);
