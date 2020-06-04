d72fd610e4c917466712f060ffdfdb94bc889b09
Spazzinq <26337405+Spazzinq@users.noreply.github.com>
2020-06-03 21:52:50 -0400

    BLM: Migrate to spazzinq.github.io

--- blm/resources/typing.js
+++
@@ -1,9 +0,0 @@
-var options = {
-     stringsElement: '.typed-strings',
-     startDelay: 1000,
-     backDelay: 2500,
-     typeSpeed: 20,
-     smartBackspace: true
- };
-
-var typed = new Typed('.typed', options);
